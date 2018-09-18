3c1d658570208d4052e6284883d44166f2eddf59
jsAdam <adamleebl@gmail.com>
2018-09-18 16:21:20 -0500

    adding site files

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("#search").on("click", function(e){
+        e.preventDefault();
+        var $input = $("input").val();
+        $.ajax({
+            method: "GET",
+            url: "http://api.giphy.com/v1/gifs/search",
+            data: {
+                api_key: myKey,
+                q: $input
+            },
+            dataType: "json"
+        }).then(function(response){
+            var length = response.data.length;
+            var randomIndex = Math.floor(Math.random()*length);
+            var $gif = $("<img>", {
+                src: response.data[randomIndex].images.original.url
+            });
+            var $area = $("#gif-area")
+            $("#gif-area").append($gif);
+            safe();
+        }).catch(function(error){
+            console.log(error);
+            safe();
+        })
+    })
+    $("#delete").on("click",function(e){
+        e.preventDefault();
+        $("#gif-area").empty();
+    })
+});
